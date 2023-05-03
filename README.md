c3505e1a1b617439d02464726e193cf32e174ed5
PeterPCW <49111837+PeterPCW@users.noreply.github.com>
2023-05-03 16:31:22 -0500

    initial version

--- README.md
+++ README.md
@@ -2,33 +2,29 @@
 
 ## Overview
 
-This is meant to be a starter kit for developing a new extension in Typescript+React. It has some styling components included and types/EsLint setup so that it *should* work out of the box on current latest versions.
+This is a simple extension that shows a form and lets you input values to start a LinkedIn search.
 
 ## Use
+
+Download the built extension files from /build/.
+
+In Chrome, go to `chrome://extensions` and click "Load unpacked" and select the `/build/chrome-mv3-prod` directory.
+
+## Or Build
 
 Install [npm](https://nodejs.org/en/download/)
 
 Sync this repo locally (Git Bash):
 
 ```bash
-git clone https://github.com/PeterPCW/typescript-react-chrome-extension
+git clone https://github.com/PeterPCW/LinkedIn-Search-From-Anywhere
 ```
 
-## Modify
-
-Run `npm install` to build the node_modules directory. THERE WILL BE ISSUES. Always are. You can use `npm install --force` or `npm audit fix` (after a successful install) to help.
+Run `npm install` to build the node_modules directory. You can use `npm install --force` or `npm audit fix` (after a successful install) to help any issues.
 
 ```cmd
 npm install
 ```
-
-Make it do whatever you want! The code here is not intended to be functional in any way.
-
-I have real extensions in other repos that use this same setup, so you can look at them for ideas or code snippets.
-
-Google or ChatGPT for other ideas and how-tos.
-
-## Build and Load
 
 Build locally (Note: this will run EsLint along the way as well):
 
@@ -38,4 +34,4 @@ pnpm build
 npm run build
 ```
 
-Then, in Chrome, go to `chrome://extensions` and click "Load unpacked" and select the `build/chrome-mv3-prod` directory.
+Then, see Use.
--- popup.tsx
+++ popup.tsx
@@ -1,15 +1,95 @@
 import React, { useState } from 'react';
+import { createGlobalStyle } from 'styled-components';
 
-function otherFunction(): string {
-  // Create other functions to do things
-  return ""
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #333333;
+    color: #878787;
+    font-weight: bold;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  input[type='text'] {
+    border: 1px solid #878787;
+    background-color: #333333;
+    color: #878787;
+  }
+
+  button {
+    color: #333333;
+    background-color: #878787;
+    font-weight: bold;
+  }
+`;
+
+interface FormState {
+  [key: string]: string;
 }
 
-function IndexPopup(): JSX.Element {
-  // Create variables and functions so that your code is interactive
-     return (
-      {/*React components and HTML code*/}
-   )
+const IndexPopup = () => {
+  const [formState, setFormState] = useState<FormState>({});
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const buildUrl = (formState: FormState) => {
+    const baseUrl = "https://www.linkedin.com/search/results/people/"; // LinkedIn Search base URL
+    const queryParams = Object.entries(formState)
+      .map(([key, value]) => {
+        const encodedValue = encodeURIComponent(value.replace(/\s/g, "%20").replace(/-/g, "%2D").replace(/\./g, "%2E").replace(/,/g, "%2C").replace(/"/g, "%22").replace(/'/g, "%27").replace(/:/g, "%3A").replace(/_/g, "%5F"));
+        return `${key}=${encodedValue}`;
+      })
+      .join("&");
+    return `${baseUrl}?${queryParams}`;
+  }  
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const url = buildUrl(formState);
+    window.open(url, "_blank"); // Open the search URL in a new tab
+  };
+  
+  return (
+    <div>
+      <GlobalStyle />
+      <form onSubmit={handleSubmit}>
+        <br /><br />
+        <label>
+          First Name:
+          <input type="text" name="firstName" onChange={handleInputChange} />
+        </label><br /><br />
+        <label>
+          Last Name:
+          <input type="text" name="lastName" onChange={handleInputChange} />
+        </label><br /><br />
+        <label>
+          Title:
+          <input type="text" name="titleFreeText" onChange={handleInputChange} />
+        </label><br /><br />
+        <label>
+          Company:
+          <input type="text" name="company" onChange={handleInputChange} />
+        </label><br /><br />
+        <label>
+          School:
+          <input type="text" name="schoolFreetext" onChange={handleInputChange} />
+        </label><br /><br />
+        <label>
+          Keywords:
+          <input type="text" name="keywords" onChange={handleInputChange} />
+        </label><br /><br />
+        {/* Add more fields as needed */}
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
